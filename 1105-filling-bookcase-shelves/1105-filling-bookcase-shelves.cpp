207f5d867bf86e6c7a1111a0720bf7afec1a63e6
Siya Bansal <siyabansal964@gmail.com>
2024-07-31 10:59:50 +0530

    Time: 3 ms (87.31%), Space: 11.1 MB (58.66%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -1,7 +1,9 @@
 class Solution {
-    public int minHeightShelves(int[][] books, int shelfWidth) {
-        int n = books.length;
-        int[] f = new int[n + 1];
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+         int n = books.size();
+        int f[n + 1];
+        f[0] = 0;
         for (int i = 1; i <= n; ++i) {
             int w = books[i - 1][0], h = books[i - 1][1];
             f[i] = f[i - 1] + h;
@@ -10,11 +12,10 @@ class Solution {
                 if (w > shelfWidth) {
                     break;
                 }
-                h = Math.max(h, books[j - 1][1]);
-                f[i] = Math.min(f[i], f[j - 1] + h);
+                h = max(h, books[j - 1][1]);
+                f[i] = min(f[i], f[j - 1] + h);
             }
         }
         return f[n];
-    
     }
-}
+};
