966296551b0b390fa21227fd713762a2efaac969
Siya Bansal <siyabansal964@gmail.com>
2024-09-02 10:16:16 +0530

    Time: 0 ms (100.00%), Space: 11.5 MB (21.02%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0,j=0,k=0;
-        vector<int> nums3=nums1;
-        while(i<m && j<n)
-        {
-            if(nums3[i]<=nums2[j])
-            {
-                nums1[k++]=nums3[i++];
-            }
-            else 
-            {
-                nums1[k++]=nums2[j++];
-            }
+        vector<int> nums3(m+n);
+        int j = 0;
+        
+        for(int i =0;i<m;i++){
+            nums3[j] = nums1[i];
+            j++;
         }
-
-        while(i<m)
-        {
-            nums1[k++]=nums3[i++];
+        
+        for(int i=0;i<n;i++){
+            nums3[j] = nums2[i];
+            j++;
         }
-
-        while(j<n)
-        {
-            nums1[k++]=nums2[j++];
+        
+        sort(nums3.begin(),nums3.end());
+        
+        for(int i=0;i<nums3.size();i++){
+            nums1[i]=nums3[i];
         }
-
-
-
     }
 };
