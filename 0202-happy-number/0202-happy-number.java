c9e4b9c541c9bdc68389d6a242d9ce33c9a059a3
Siya Bansal <siyabansal964@gmail.com>
2024-09-09 19:29:08 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (87.21%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,33 +1,26 @@
 class Solution {
-    public boolean isHappy(int n) {
-        
-        int slow = n;
-        int fast = n;
-
-        do {
-            slow = square(slow);           // Move slow one step
-            fast = square(square(fast));    // Move fast two steps
-            
-            // If either slow or fast reaches 1, then the number is happy
-            if (slow == 1 || fast == 1) {
-                return true;
-            }
-        } while (slow != fast); // Stop when slow meets fast
-        
-        // If the loop terminates without reaching 1, it's not a happy number
-        return false;
+    // Helper method to compute the sum of squares of digits of a number
+    int sumOfDigit(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
     }
 
-    public int square(int num) {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
         
-        int ans = 0;
+        // Floyd's cycle detection algorithm
+        do {
+            slow = sumOfDigit(slow);         // Move slow by 1 step
+            fast = sumOfDigit(sumOfDigit(fast));  // Move fast by 2 steps
+        } while (slow != fast);
         
-        while(num > 0) {
-            int remainder = num % 10;
-            ans += remainder * remainder;
-            num /= 10;
-        }
-        
-        return ans;
+        // If both slow and fast pointers meet at 1, then it is a happy number
+        return slow == 1;
     }
 }
