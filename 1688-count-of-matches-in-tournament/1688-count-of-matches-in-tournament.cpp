3859b21d7c31c0ba8949a07976ec13bba6529542
Siya Bansal <siyabansal964@gmail.com>
2024-11-16 12:27:59 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (48.90%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        return n-1;
+        int answer = 0;
+        
+        while(n>1){
+            answer += n/2;
+            
+            if(n%2==0){
+                n = n/2;
+            }
+            else{
+                n = (n/2) + 1;
+            }
+        }
+        return answer;
     }
 };
