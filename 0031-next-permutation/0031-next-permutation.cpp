403110f68fab6e9c25a1dee81c823c7520723318
Siya Bansal <siyabansal964@gmail.com>
2024-11-16 19:15:20 +0530

    Time: 0 ms (100.00%), Space: 14.8 MB (80.56%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,7 +4,6 @@ public:
        int n = nums.size();
        int index = -1;
         
-        // Find the first index from the end where nums[i] < nums[i + 1]
         for(int i = n - 2; i >= 0; i--) {
             if(nums[i] < nums[i + 1]) {
                 index = i;
@@ -13,21 +12,17 @@ public:
         }
         
         if(index == -1) {
-            // If no such index is found, reverse the whole array
             reverse(nums.begin(), nums.end());
         } 
         
         else {
-            // Find the smallest number greater than nums[index] to the right of index
             int j = n - 1;
             while (j > index && nums[j] <= nums[index]) {
                 j--;
             }
             
-            // Swap the numbers at index and j
             swap(nums[index], nums[j]);
             
-            // Reverse the numbers to the right of index
             reverse(nums.begin() + index + 1, nums.end());
         }
     }
