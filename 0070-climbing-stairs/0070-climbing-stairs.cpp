f261edc63e029d2bfbac8ba76241c5283dcab316
Siya Bansal <siyabansal964@gmail.com>
2024-11-19 16:45:22 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (66.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 1) return 1;
-
-        int prev1 = 1, prev2 = 1;
-        for (int i = 2; i <= n; i++) {
-            int current = prev1 + prev2; 
-            prev2 = prev1;
-            prev1 = current;
+        if(n==1)return 1;
+        if(n==2)return 2;
+        
+        int prev1 = 1;
+        int prev2 = 2;
+        
+        for(int i=3;i<=n;i++){
+            int current = prev1+prev2;
+            prev1 = prev2;
+            prev2= current;
         }
-
-        return prev1; // f(n) is stored in prev1
+        
+        return prev2;
     }
 };
