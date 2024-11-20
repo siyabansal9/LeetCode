3fe83704fa4e25decb85a0e19261fb083fe49ffd
Siya Bansal <siyabansal964@gmail.com>
2024-11-20 20:09:12 +0530

    Time: 3 ms (70.19%), Space: 53.7 MB (50.42%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -5,9 +5,9 @@ class Solution {
         int[] inDegree = new int[n+1];
         int[] outDegree = new int[n+1];
         
-        for (int[] t : trust) {
-            int a = t[0];
-            int b = t[1];
+        for (int i=0;i<trust.length;i++) {
+            int a = trust[i][0];
+            int b = trust[i][1];
             outDegree[a]++;
             inDegree[b]++;
         }
