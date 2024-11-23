08a3ab017bdbdfd9b83eb8df5c3471b7ce15e05a
Siya Bansal <siyabansal964@gmail.com>
2024-11-23 10:23:27 +0530

    Time: 3 ms (30.63%), Space: 14.3 MB (13.58%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     
-    vector<vector<int>> subsetHelper(vector<int>& nums, int i) {
-        if(i==nums.size()) return {{}};
-        
-        vector<vector<int>> partial_ans = subsetHelper(nums,i+1);
-        vector<vector<int>> dup = partial_ans;
-        
-        for(auto &v : dup){
-            v.push_back(nums[i]);
+    vector<vector<int>> ans;
+    
+    void subsetHelper(vector<int>& nums, int i, vector<int> subset) {
+        if(i==nums.size()){
+            ans.push_back(subset);
+            return;
         }
         
-        vector<vector<int>> ans;
-        for(auto a : partial_ans) ans.push_back(a);
-        for(auto b : dup) ans.push_back(b);
+        subset.push_back(nums[i]);
+        subsetHelper(nums,i+1,subset);
+        subset.pop_back();
+        subsetHelper(nums,i+1,subset);
         
-        return ans;
+        
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans = subsetHelper(nums,0);
+        vector<int> subset;
+        subsetHelper(nums,0,subset);
+        
+        return ans;
         
         return ans;
     }
